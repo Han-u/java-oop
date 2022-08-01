706a0fb971dcf59ed5529df21901bfcfe3806696
han <chos9715@naver.com>
2022-08-01 23:36:15 +0900

    ~3-7강 math,date

---
+++ src/Math.md
@@ -0,0 +1,5 @@
+# Math
+## 소수점 처리 함수
+- round : 반올림
+- ceil: 올림
+- floor: 버림
---
+++ src/메스테스트.java
@@ -0,0 +1,8 @@
+public class 메스테스트 {
+    public static void main(String[] args){
+        double d1 = 12.426;
+        System.out.println(Math.round(d1));
+        System.out.println(Math.ceil(d1));
+        System.out.println(Math.floor(d1));
+    }
+}
